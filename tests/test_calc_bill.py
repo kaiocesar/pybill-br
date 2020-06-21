9fa3c31ef053f78313eb93df9dde9dfecaf8b679
Kaio Cesar <tecnico.kaio@gmail.com>
2020-06-21 17:16:52 -0300

    TDD red part 2

--- tests/test_calc_bill.py
+++ tests/test_calc_bill.py
@@ -7,21 +7,35 @@ class TestCalcBill:
         assert 1 == 1
 
     def test_calc_inss_range_1(self):
-        paybill1 = PyBill(salary=1751.82)
-        assert CalculateBill.calculate(paybill1, INSS()), 140.1456
+        paybill = PyBill(salary=1751.82)
+        assert CalculateBill.calculate(paybill, INSS()), 140.1456
 
     def test_calc_inss_range_2(self):
-        pybill2 = PyBill(salary=1751.83)
-        assert CalculateBill.calculate(paybill2, INSS()), 140.1464
+        paybill = PyBill(salary=1751.83)
+        assert CalculateBill.calculate(paybill, INSS()), 140.1464
 
     def test_calc_inss_range_3(self):
-        pybill3 = PyBill(salary=2919.73)
-        assert CalculateBill.calculate(paybill3, INSS()), 321.1703
+        paybill = PyBill(salary=2919.73)
+        assert CalculateBill.calculate(paybill, INSS()), 321.1703
 
     def test_calc_inss_range_4(self):
-        pybill4 = PyBill(salary=5839.46)
-        assert CalculateBill.calculate(paybill4, INSS()), 642.34
+        paybill = PyBill(salary=5839.46)
+        assert CalculateBill.calculate(paybill, INSS()), 642.34
 
     def test_calc_inss_with_zero_salary(self):
-        pybill5 = PyBill(salary=0)
-        assert CalculateBill.calculate(paybill5, INSS()), 0
+        paybill = PyBill(salary=0)
+        assert CalculateBill.calculate(paybill, INSS()), 0
+
+
+    def test_calc_ftgs_with_valid_salary(self):
+        paybill = PyBill(salary=2000)
+        assert CalculateBill.calculate(paybill, FGTS()), 160
+
+
+    def test_calc_irrf_range_1(self):
+        paybill = PyBill(salary=1903.98)
+        assert CalculateBill.calculate(paybill, IRRF()), 0
+
+    def test_calc_irrf_range_2(self):
+        paybill = PyBill(salary=2826.66)
+        assert CalculateBill.calculate(paybill, IRRF()), 0
